3bd78cb091534c3c2a17fd1885ef90b52975d708
psb4728 <63276761+psb4728@users.noreply.github.com>
2022-04-11 13:09:42 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,41 +1,6 @@
-// Scroll Event
-const scroll = window.requestAnimationFrame || function(callback){ window.setTimeout(callback, 1000/60)};
-const elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+$(function () {
+  $("#header").load("../common/header.html");
+  $("#footer").load("../common/footer.html");
+});
 
-function loop() {
-  Array.prototype.forEach.call(elementsToShow, function(element){
-    if (isElementInViewport(element)) {
-      element.classList.add('is-visible');
-    } 
-  });
-
-  scroll(loop);
-}
-
-loop();
-
-function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 200
-      && rect.bottom >= 200)
-    ||
-    (rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top >= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 200 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
-}
-
-const navigation = document.querySelector('.navigation');
-const hamBtn = document.querySelector('.ham_btn');
-
-hamBtn.addEventListener('click', ()=> {
-  hamBtn.classList.toggle('open');
-  navigation.classList.toggle('open');
-})
-// menuParent.addEventListener('click', () => {
-//   subMenu.classList.toggle('open');
-// });
 
